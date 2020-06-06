753e4901d7bb14448e3ac3439dc6fcf885515b8c
unknown <Crest@W147-DhavalB.CDSYS.LOCAL>
2020-06-06 11:40:15 +0530

    created node project with rendering

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const path = require("path")
+const hoganMiddleware = require("hogan-middleware")
+
+const app = express()
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'mustache')
+app.engine("mustache", hoganMiddleware.__express)
+
+app.get('/', (req, res, next) => {
+    res.send('Hello')
+})
+
+app.get('/json', (req, res, next) => {
+    let a = {
+        hello: "greeting"
+    }
+    res.json(a)
+})
+
+app.get('/home', (req, res, next) => {
+    res.render('home', null)
+})
+
+app.listen(3000)
