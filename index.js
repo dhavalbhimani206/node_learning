479cafe3860caa94b00e3103ffc1f409a90837c0
dhavalbhimani1 <dhaval.bhimani@crestdatasys.com>
2021-04-26 19:45:11 +0530

    Added support of HTTPS

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require("express")
 const path = require("path")
 const hoganMiddleware = require("hogan-middleware")
 
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
+
+
+var https = require('https');
+
+
 const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'mustache')
@@ -12,7 +23,9 @@ app.use(express.static(path.join(__dirname, "public")))
 
 const indexRouter = require("./routes/index")
 app.use('/', indexRouter)
+// app.listen(3000)
 
-app.listen(3000)
+var httpsServer = https.createServer(options, app);
+httpsServer.listen(443, "0.0.0.0");
 
 console.log('Server is runnning')
